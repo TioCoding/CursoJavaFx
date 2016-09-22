951d474fc22ed09924cdbb2a6eeb64d882a9ebc3
TioCoding <tioCoding@gmail.com>
2016-09-22 02:10:23 -0500

    Creando una primera vista
    Pequeño ejemplo de una vista, y de como ejecutarla

--- src/main/java/com/javafx/tiocoding/main/Main.java
+++ src/main/java/com/javafx/tiocoding/main/Main.java
@@ -1,6 +1,9 @@
 package com.javafx.tiocoding.main;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application{
@@ -8,6 +11,11 @@ public class Main extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
+		Parent root = FXMLLoader.load(getClass().getResource("/view/inicio.fxml"));
+		Scene scene = new Scene(root);
+		
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 
 	public static void main(String[] args){
